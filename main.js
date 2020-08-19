881f45e80c9fdd616f4a210fd383e5100ca1de65
A-S-Maddy <foxxtar.styles@gmail.com>
2020-08-20 01:07:18 +0530

    commit no.6 ,Finished,Ready for testing

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 window.onload = function () {
-
-setTimeout(()=>{document.querySelector('#load').style.display = 'none';
-   document.querySelector('.player_body').style.display = 'block';},5600);
-    
+//
+//setTimeout(()=>{document.querySelector('#load').style.display = 'none';
+//   document.querySelector('.player_body').style.display = 'block';},5600);
+//   
     
     //Variables
-    let audioArray = ["songs/Benny%20Blanco%20Halsey%20&%20Khalid-Eastside.mp3",
+    let audioArray = ["songs/Ed%20Sheeran%20-%20Perfect%20[Mw%20Hits].mp3",
+                      
+"songs/Imran%20Khan%20-%20Satisfya%20(Official%20Music%20Video)%20(1).mp3",
+                      
+"songs/Alan%20Walker,%20Ava%20Max%20-%20Alone,%20Pt.%20II.mp3",
+                      
+"songs/Diplo_-_Revolution_feat_Faustix_Imanos_and_Kai_Official_Music_Video%5BSaveFrom.online%5D.mp3", 
+                      
+"songs/Alessia%20Cara%20-%20How%20Far%20I'll%20Go%20(Official%20Video).mp3" ,
+"songs/Benny%20Blanco%20Halsey%20&%20Khalid-Eastside.mp3","songs/Pal%20Pal%20Dil%20Ke%20Paas%20Title%20Arijit%20Singh%20Karan%20Deol,%20Sahher%20Parampara,%20Sachet,%20Rishi%20Rich.mp3", "songs/Loud%20Luxury%20Ft.%20Brando-Body.mp3", "songs/Imran%20Khan%20-%20Amplifier%20(Official%20Music%20Video).mp3"
 
-        "songs/Alan%20Walker,%20Ava%20Max%20-%20Alone,%20Pt.%20II.mp3", "songs/Diplo_-_Revolution_feat_Faustix_Imanos_and_Kai_Official_Music_Video%5BSaveFrom.online%5D.mp3", "songs/Ed%20Sheeran%20-%20Perfect%20[Mw%20Hits].mp3", "songs/Loud%20Luxury%20Ft.%20Brando-Body.mp3", "songs/Ava%20Max%20-%20Sweet%20but%20Psycho.mp3", "songs/Shawn%20Mendes-Treat%20You%20Better.mp3"
     ];
-    let imgArray = ["img/eastside.jpg", "img/external-content.duckduckgo.com.jpg", "img/re.jpg", "img/Ed-Sheeran-Divide Cover.jpg", "img/body.jpg", "img/Cover%20Ava%20Max%20-%20Sweet%20but%20Psycho.jpg", "img/treat%20u%20better.jpg"];
+    let imgArray = ["img/Ed-Sheeran-Divide Cover.jpg","img/satisfya.jpg", "img/external-content.duckduckgo.com.jpg", "img/kai.jpg", "img/Alessia%20cara.jpg","img/eastside.jpg", "img/Arijit.jpg","img/body.jpg", "img/amplifier.jpg"];
 
-    let nameArray = ["Eastside", "Alone Pt.2", "Revolution", "Perfect", "Body", "Sweet but Psycho", "Treat You Better"];
+    let nameArray = ["Perfect", "Satisfya","Alone Pt.2","Revolution","How Far I'll Go", "Eastside","Pal pal dil ke pas", "Body", "Amplifier"];
 
-    let artistArray = ["Benny Blanco, Halsey & Khalid", "Alan Walker ft.Ava Max", "Diplo ft. Kai & Faustix Imanos", "Ed Sheeran", "Loud Luxury ft. Brando", "Ava max", "Shawn Mendes"];
+    let artistArray = ["Ed Sheeran","Imran Khan", "Alan Walker ft.Ava Max", "Diplo ft. Kai & Faustix Imanos","Alessia Cara", "Benny Blanco, Halsey & Khalid","Arijit Singh", "Loud Luxury ft. Brando", "Imran Khan"];
     const showcase = document.querySelector('.display');
     const display = document.querySelector('.display_inner');
     const rollUp = document.querySelector('.fa-list');
-    const theme = document.querySelector('.fa-paint-brush');
     const left = document.querySelector('.fa-chevron-left');
     const right = document.querySelector('.fa-chevron-right');
     let button = document.querySelectorAll('.play')[0];
     let active = false;
     let songDisplay = document.querySelector('.currentSong');
+    const timer = document.querySelector('.currentTime');
     let seekBar = document.querySelector('input[type=range]');
     let seek, stop;
     let listView = document.querySelector('.listView');
@@ -99,7 +107,7 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
             lisArray.push(li);
             uList.appendChild(li);
         }
-        listView.appendChild(uList)
+        listView.appendChild(uList);
         return lisArray;
     }
     mainPlayer.prototype = Object.create(createMusic.prototype);
@@ -123,13 +131,6 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
     mainPlayer.prototype.imgPlaying = function (j) {
 
         stage.push(j)
-//        left.onclick = function () {
-//            stage.push(parseInt(j, 10) - 1)
-//        };
-//
-//        right.onclick = function () {
-//            stage.push(parseInt(j, 10) + 1)
-//        };
         stage.length > 2 ? stage.splice(0, 1) : console.log(stage);
 
         console.log(active);
@@ -144,7 +145,7 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
         } else if (active == true) {
             this.music[stage[0]].currentTime = 0;
             this.music[stage[0]].pause();
-            this.music[j].play();
+            this.music[stage[1]].play();
             button.setAttribute('class', ' fa fa-pause-circle-o play');
             songDisplay.innerHTML = `<p>${this.artist[j].childNodes[0].innerText}</p> <p> ${this.artist[j].childNodes[1].innerText}</p>`;
             return active;
@@ -154,11 +155,8 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
 
     }
     mainPlayer.prototype.moveSong = function (j) {
-    console.log(this.music[j]);
-        if(this.music[j].currentTime == 2){
-            console.log(stage[1]+1);
-        }
-        
+    console.log(this.music[j].duration);
+        return this.music[j];  
     }
     mainPlayer.prototype.utility = function (j) {
         console.log(active);
@@ -170,7 +168,7 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
             this.music[0].onended = function () {
                 console.log(this.music[j]);
             }
-        } else if (active == true && stage.length >= 1) {
+        } else if (active == true) {
             active = false;
             this.music[j].pause();
             button.setAttribute('class', ' fa fa-play-circle-o play');
@@ -191,9 +189,16 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
                 }
                 seek = setInterval(function () {
                     seekBar.value = presentSong.currentTime;
-                    if(presentSong.currentTime==5){
-                        player.imgPlaying(stage[1]+1);
-                    }
+                    date = new Date(presentSong.currentTime*1000);
+                    let minute = date.getUTCMinutes();
+                    let sec = date.getUTCSeconds();
+                    timer.innerText = `${minute}:${sec}`;
+                    let val = seekBar.value/seekBar.max;
+                    let v = document.documentElement.style.setProperty('--seekPos',val);
+                    seekBar.style.backgroundImage = -"webkit-gradient(linear,left top,right top,"+
+                        "color-stop("+v+", yellow),"+"color-stop("+v+", black)"+")"; 
+
+                    
                 }, 1000);
 
                 function stopSeek() {
@@ -205,16 +210,20 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
                 seekBar.onchange = function () {
                     presentSong.currentTime = seekBar.value;
                     seekBar.value = presentSong.currentTime;
+                    let val = seekBar.value/seekBar.max;
+                    let v = document.documentElement.style.setProperty('--seekPos',val);
+                    seekBar.style.backgroundImage = -"webkit-gradient(linear,left top,right top,"+
+                        "color-stop("+v+", yellow),"+"color-stop("+v+", black)"+")"; 
                 }
-                function end(){
-                    console.log(presentSong.currentTime)
-                    if(presentSong.ended){
-                        console.log(stage+' '+ended);
-                        active == false;
-                        
-                    }
-                }
-                requestAnimationFrame()
+//                function end(){
+//                    console.log(presentSong.currentTime)
+//                    if(presentSong.ended){
+//                        console.log(stage+' '+ended);
+//                        active == false;
+//                        
+//                    }
+//                }
+//                
 
             } catch (err) {
                 console.log(err);
@@ -226,108 +235,119 @@ setTimeout(()=>{document.querySelector('#load').style.display = 'none';
     const play = new createMusic(audioArray, imgArray, nameArray, artistArray);
 
     const player = new mainPlayer(play.createPlayer(), play.createImg(), play.Title(), play.list());
-    let im = document.querySelectorAll('.play_img');
+    let imageSelect = document.querySelectorAll('.play_img');
     let list = document.querySelectorAll('.list');
-    for (let j in audioArray) {
-       
-        im[j].onclick = function () {
-            player.imgPlaying(j);
+    let selected;
+    //main method
+    function main(pos){
+        player.imgPlaying(pos);
             clearInterval(seek);
-            player.seekable(j);
-            showcase.setAttribute('id', 'big-display');
-            im[j].setAttribute('id', 'big-img');
-            rollUp.style.visibility = 'visible';
-            theme.style.display = ' none';
+             player.seekable(pos);
+            selected = player.moveSong(pos);
             
-            if (stage.length > 1) {
-                im[stage[0]].removeAttribute('id', 'big-img');
-
-                showcase.scrollLeft += im[j].getBoundingClientRect().x - 30;
-            } else {
-                showcase.scrollLeft += im[j].getBoundingClientRect().x - 30;
-            }
+// selected song Image slide animations
             rollUp.onclick = function () {
                 showcase.removeAttribute('id', 'big-display');
-                im[j].removeAttribute('id', 'big-img');
+                imageSelect[pos].removeAttribute('id', 'big-img');
                 rollUp.style.visibility = 'hidden';
-                theme.style.display = 'block';
+              
             }
             button.onclick = function () {
-                player.utility(j);
+                player.utility(pos);
             }
             
         
     }
+    let imgSelector = (pos) => {                     
+        showcase.setAttribute('id', 'big-display');
+        imageSelect[pos].setAttribute('id', 'big-img');
+        rollUp.style.visibility = 'visible';
+        
+                                
+        if (stage.length > 1) {
+         imageSelect[stage[0]].removeAttribute('id', 'big-img');
+
+        showcase.scrollLeft += imageSelect[pos].getBoundingClientRect().x - 30;
+            } else {
+                showcase.scrollLeft += imageSelect[pos].getBoundingClientRect().x - 30;
+            }
+            
+    }
+    let imgSlider = (pos) => {
+                    showcase.scrollLeft += imageSelect[pos].getBoundingClientRect().x - 100;
+                    
+    }
+    for (let j in audioArray) {
+   
+        imageSelect[j].onclick = function () {
+            main(j);
+            imgSelector(j)
+            
+            selected.onended = () => {
+                console.log(stage);
+                j = parseInt(j,10)+1
+                main(j);
+                imgSelector(j);
+            }
+            left.onclick = function(){
+                j = parseInt(j,10)-1
+                main(j);
+                imgSelector(j);
+            }
+            right.onclick = function(){
+                j = parseInt(j,10)+1
+                main(j);
+                imgSelector(j);
+            }
+
+    }
 
         list[j].onclick = function () {
-            
-        left.onclick = function () {
-            if (stage.length > 1) {
-               j-=1;
-                player.imgPlaying(j);
-                 button.onclick = function () {
-                player.utility(j);
+            main(j);
+            imgSlider(j);
+            imageSelect[j].onclick = () => imgSelector(j);
+            selected.onended = () => {
+                console.log("well well");
+                j = parseInt(j,10)+1
+                main(j);
+                imgSlider(j);
             }
+            left.onclick = function(){
+                j = parseInt(j,10)-1
+                main(j);
+                imgSlider(j);
             }
-        };
-        right.onclick = function () {
-            if (stage.length > 1) {
-               j+=1;
-                player.imgPlaying(j);
-                 button.onclick = function () {
-                player.utility(j);
-            }
-            }
-        };
-            player.imgPlaying(j);
-            clearInterval(seek);
-            player.seekable(j);
-            showcase.scrollLeft += im[j].getBoundingClientRect().x - 100;
-
-            button.onclick = function () {
-                player.utility(j);
-            }
-                left.onclick = function () {
-                    player.imgPlaying(parseInt(j, 10) - 1)
-                };
-
-                right.onclick = function () {
-                    player.imgPlaying(parseInt(j, 10) + 2)
-                };
-
-            
-            button.onclick = function () {
-                player.utility(j);
+            right.onclick = function(){
+                j = parseInt(j,10)+1
+                main(j);
+                imgSlider(j);
             }
         }
-        console.log(stage);
-//        left.onclick = function () {
-//            if (stage.length > 1) {
-//               j=stage[1]-1;
-//                console.log(stage)
-//                player.imgPlaying(j);
-//            }
-//        };
-//        right.onclick = function () {
-//            if (stage.length > 1) {
-//               j = stage[1]+1;
-//                console.log(stage);
-//                player.imgPlaying(j);
-//            }
- }
-      
-    for(let i = 0;i<=list.length;i++){
-        theme.onclick = function () {
-        if (theme.style.color == 'yellow') {
-            listView.style.backgroundColor = 'rgb(212,241,19)';
-            list[i].setAttribute( 'id','secondShadow');
-            theme.style.color = 'maroon';
-        } else {
-            listView.style.backgroundColor = 'maroon';
-            list[i].removeAttribute( 'id','secondShadow');
-            theme.style.color = 'yellow';
+            
+     if(active == false && stage.length == 0){
+         button.onclick = function(j){
+             j = 0
+            main(j);
+            imgSelector(j)
+            selected.onended = () => {
+                console.log("well well");
+                j = parseInt(j,10)+1
+                main(j);
+                imgSelector(j);
             }
-        }
+            left.onclick = function(){
+                j = parseInt(j,10)-1
+                main(j);
+                imgSelector(j);
+            }
+            right.onclick = function(){
+                j = parseInt(j,10)+1
+                main(j);
+                imgSelector(j);
+            }
+ 
+         }
+     } 
     }
     
 
